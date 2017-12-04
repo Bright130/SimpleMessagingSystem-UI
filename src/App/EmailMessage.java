c3b2350bc837965da992c9f165cff6dc8f183e4d
DESKTOP-5EHLS6M\pawee <paween.jabberry@gmail.com>
2017-12-04 22:49:01 +0700

    Update increase Comment EmailMessage and IOUtils class

--- src/App/EmailMessage.java
+++ src/App/EmailMessage.java
@@ -1,8 +1,23 @@
+/**
+ *    EmailMessage
+ *      This class have member variables and methods for create an email
+ *
+ *   Created by Chainarong Tumapha (Bright)  58070503409 AND
+ *              Paween Surimittragool (Jarb) 58070503457
+ *
+ *       Group BJ
+ *       24 Oct. 2017
+ */
+
 package App;
 import java.lang.String;
 
 public class EmailMessage
 {
+    /**
+     *  The member variables.
+     */
+
     private int id;
 
     private String lastModified;
@@ -25,6 +40,9 @@ public class EmailMessage
     {
     }
 
+    /**
+     *  The constructor for create new email
+     */
     EmailMessage(String lastModified,String toAddress,String fromAddress,String subject,String bodyText,int isRead,int isSenderDel,int isReaderDel)
     {
         this.lastModified = lastModified;
@@ -37,6 +55,9 @@ public class EmailMessage
         this.isSenderDel = isSenderDel;
     }
 
+    /**
+     *  The constructor for create email that using in database
+     */
     EmailMessage(int id,String lastModified,String toAddress,String fromAddress,String subject,String bodyText,int isRead,int isSenderDel,int isReaderDel){
         this.id = id;
         this.lastModified = lastModified;
@@ -49,55 +70,98 @@ public class EmailMessage
         this.isSenderDel = isSenderDel;
     }
 
+    /**
+     *  The methods
+     */
+
+    /**
+     *  Get Integer from changed in DashboardController for set setIsRead.
+     */
     public void setIsRead(int isRead)
     {
         this.isRead = isRead;
     }
 
+    /**
+     *  Get Integer from deleteMessage in DashboardController for set isReaderDel.
+     */
     public void setIsReaderDel(int delRead)
     {
         this.isReaderDel = delRead;
     }
+
+    /**
+     *  Get Integer from deleteMessage in DashboardController for set isSenderDel.
+     */
     public void setIsSenderDel(int delSend)
     {
         this.isSenderDel = delSend;
     }
 
+    /**
+     *  Return id that use for database
+     */
     public int getId() { return id; }
 
+    /**
+     *  Return the last date of user's login
+     */
     public String getLastModified()
     {
         return lastModified;
     }
 
+    /**
+     *  Return ToEmail of this email
+     */
     public String getToEmail()
     {
         return toAddress;
     }
 
+    /**
+     *  Return FromEmail of this email
+     */
     public String getFromEmail()
     {
         return fromAddress;
     }
 
+    /**
+     *  Return Subject of this email
+     */
     public String getSubject()
     {
         return subject;
     }
 
+    /**
+     *  Return BodyText of this email
+     */
     public String getBodyText()
     {
         return bodyText;
     }
 
+    /**
+     *  Return the status of Read Email(Unread/read)
+     */
     public int getIsRead()
     {
         return isRead;
     }
+
+    /**
+     *  Return the status of Delete from Reader
+     */
     public int getIsReaderDel()
     {
         return isReaderDel;
     }
+
+    /**
+     *  Return the status of Delete from Sender
+     */
     public int getIsSenderDel()
     {
         return isSenderDel;
--- src/App/IOUtils.java
+++ src/App/IOUtils.java
@@ -1,3 +1,14 @@
+/**
+ *    IOUtils
+ *       A class that check the pattern of email and password and return machine's time
+ *
+ *   Created by Chainarong Tumapha (Bright)  58070503409 AND
+ *              Paween Surimittragool (Jarb) 58070503457
+ *
+ *       Group BJ
+ *       24 Oct. 2017
+ */
+
 package App;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -6,7 +17,11 @@ import java.util.regex.Pattern;
 
 public class IOUtils
 {
-
+    /**
+     * Check the email that correctly following the pattern form "a-z,A-Z,0-9:@:a-z,A-Z,0-9:.:a-z,A-Z{1-6}"
+     * @param   email   String of email
+     * @return true if the email correctly following the pattern if not return false
+     */
     public static boolean checkEmail(String email)
     {
         Pattern p = Pattern.compile("^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$");
@@ -21,6 +36,11 @@ public class IOUtils
         return true;
     }
 
+    /**
+     * Check the email that correctly following the pattern form should be 8-12 character
+     * @param   password   String of password
+     * @return true if the password correctly following the pattern if not return false
+     */
     public static boolean checkPassword(String password)
     {
         if(password.length()<8 || password.length()>12)
